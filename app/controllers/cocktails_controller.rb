a57cc1bc2062a31568f3301d9a95eeb74885538f
steven van eecke <stevenvaneecke@hotmail.com>
2018-11-06 16:48:07 +0800

    fix

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -31,7 +31,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :photo_cache)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 
